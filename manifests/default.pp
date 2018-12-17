2922cb17855fae87a2782bd74c017e6ef2b611ed
Thomas Bennett <tbenz9@gmail.com>
2018-12-17 14:46:31 -0800

    removed bad stanza

--- manifests/default.pp
+++ manifests/default.pp
@@ -34,13 +34,6 @@ ssh_authorized_key { 'tbenz9@raspberrypi':
         key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDB2mDzCcidSi6/M+MxFcWG0J1MEgJ1reKrSGbRBdJLNW5/SzO84S2GxLptZjPOCyvXZdbem4JdyY22sXW4z4a9v/IaS03I+a5w3zDzW0SRLAg7qG6gSJrpfLKn2isxaV1A4xqxGtnqHt1lLZYUSd/m6GmMS/9rTsLJSSPzVcbMzXr51YtusbytLQFDWLLdcchn2eR4rHVuGqq3q2jRaR63kp5hnaQZDWku3uRYGzenjGzwUT2NitS3UNMPdFvnHNnxCFzNMBl9k6kfe8ZObj+NcKS992W6drkTYDSEikXZAEkqY1spM+1GK4dMginT171EnBbrtbYwn4OYNf2dc+dH',
 }
 
-ssh_authorized_key { 'tbenz9@raspberrypi2':
-        name => 'tbenz9@raspberrypi',
-        ensure => 'present',
-        type => 'ssh-rsa',
-        user => 'tbenz9',
-        key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDB2mDzCcidSi6/M+MxFcWG0J1MEgJ1reKrSGbRBdJLNW5/SzO84S2GxLptZjPOCyvXZdbem4JdyY22sXW4z4a9v/IaS03I+a5w3zDzW0SRLAg7qG6gSJrpfLKn2isxaV1A4xqxGtnqHt1lLZYUSd/m6GmMS/9rTsLJSSPzVcbMzXr51YtusbytLQFDWLLdcchn2eR4rHVuGqq3q2jRaR63kp5hnaQZDWku3uRYGzenjGzwUT2NitS3UNMPdFvnHNnxCFzNMBl9k6kfe8ZObj+NcKS992W6drkTYDSEikXZAEkqY1spM+1GK4dMginT171EnBbrtbYwn4OYNf2dc+dH',
-}
 # packages
 package { 'golang':
         name => 'golang-go',
