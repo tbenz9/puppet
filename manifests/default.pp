04faf21cf529a6c4949c4365ecb9776281ba8bdc
Thomas Bennett <tbenz9@gmail.com>
2019-04-26 12:50:48 -0700

    adding another key

--- manifests/default.pp
+++ manifests/default.pp
@@ -27,11 +27,19 @@ file { '/home/tbenz9/.ssh':
         recurse => 'true',
 }
 
-ssh_authorized_key { 'tbenz9@raspberrypi':
-        name => 'tbenz9@raspberrypi',
+ssh_authorized_key { 'tbenz9@raspberrypi-to-root':
+        name => 'tbenz9-to-root',
         ensure => 'present',
         type => 'ssh-rsa',
         user => 'root',
+        key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDB2mDzCcidSi6/M+MxFcWG0J1MEgJ1reKrSGbRBdJLNW5/SzO84S2GxLptZjPOCyvXZdbem4JdyY22sXW4z4a9v/IaS03I+a5w3zDzW0SRLAg7qG6gSJrpfLKn2isxaV1A4xqxGtnqHt1lLZYUSd/m6GmMS/9rTsLJSSPzVcbMzXr51YtusbytLQFDWLLdcchn2eR4rHVuGqq3q2jRaR63kp5hnaQZDWku3uRYGzenjGzwUT2NitS3UNMPdFvnHNnxCFzNMBl9k6kfe8ZObj+NcKS992W6drkTYDSEikXZAEkqY1spM+1GK4dMginT171EnBbrtbYwn4OYNf2dc+dH',
+}
+
+ssh_authorized_key { 'tbenz9@raspberrypi-to-tbenz9':
+        name => 'tbenz9-to-tbenz9',
+        ensure => 'present',
+        type => 'ssh-rsa',
+        user => 'tbenz9',
         key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDB2mDzCcidSi6/M+MxFcWG0J1MEgJ1reKrSGbRBdJLNW5/SzO84S2GxLptZjPOCyvXZdbem4JdyY22sXW4z4a9v/IaS03I+a5w3zDzW0SRLAg7qG6gSJrpfLKn2isxaV1A4xqxGtnqHt1lLZYUSd/m6GmMS/9rTsLJSSPzVcbMzXr51YtusbytLQFDWLLdcchn2eR4rHVuGqq3q2jRaR63kp5hnaQZDWku3uRYGzenjGzwUT2NitS3UNMPdFvnHNnxCFzNMBl9k6kfe8ZObj+NcKS992W6drkTYDSEikXZAEkqY1spM+1GK4dMginT171EnBbrtbYwn4OYNf2dc+dH',
 }
 
