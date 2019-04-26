b3da0525b1a7ff45eef2f01274e87e295896e3dc
Thomas Bennett <tbenz9@gmail.com>
2019-04-26 11:39:53 -0700

    fix goroot

--- manifests/default.pp
+++ manifests/default.pp
@@ -59,6 +59,6 @@ class { 'golang':
   base_dir    => '/usr/local/go',
   from_repo   => true,
   repo_version => 'go1.12',
-  goroot      => '$GOPATH/bin:/usr/local/go/bin:$PATH',
+  goroot      => '',
   workdir     => '/home/tbenz9/go',
 }
